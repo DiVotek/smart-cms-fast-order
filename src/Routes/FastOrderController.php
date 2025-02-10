02477b307a9a2eeef2bfa28777d5f8eaea042827
maxboyko <maxboiko@divotek.com>
2025-02-10 13:27:58 +0200

    fix fast orders

--- src/Routes/FastOrderController.php
+++ src/Routes/FastOrderController.php
@@ -9,36 +9,42 @@ use SmartCms\Core\Repositories\Field\FieldRepository;
 use SmartCms\Core\Services\ScmsResponse;
 use SmartCms\FastOrders\Models\FastOrder;
 use SmartCms\Store\Models\OrderStatus;
+use SmartCms\Store\Models\Product;
 
 class FastOrderController
 {
     public function add(Request $request)
     {
-
-        $validator = Validator::make($request->all(), [
-            'product_id' => 'required|exists:SmartCms\Store\Models\Product,id',
-            'data' => 'required|array',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'errors' => $validator->errors(),
-            ], 422);
+        $validation = [
+            'product_id' => 'required|exists:' . Product::getDb() . ',id',
+        ];
+        $fastOrderFields = setting('fastorder.fields', []);
+        $customAttributes = [];
+        foreach ($fastOrderFields as $field) {
+            $field = Field::query()->find($field['field_id']);
+            if ($field->required) {
+                $validation[strtolower($field->html_id)] = 'required';
+            }
+            if ($field->validation) {
+                $validation[strtolower($field->html_id)] = $field->validation;
+            }
+            $customAttributes[strtolower($field->html_id)] = $field->label[current_lang()] ?? $field->label[main_lang()] ?? '';
         }
-
-        $fastOrder = FastOrder::create([
+        $validator = Validator::make($request->all(), $validation);
+        $validator->setAttributeNames($customAttributes);
+        if ($validator->fails()) {
+            return new ScmsResponse(false, [], $validator->errors()->toArray());
+        }
+        FastOrder::query()->create([
             'product_id' => $request->product_id,
-            'order_status_id' => OrderStatus::query()->first(),
-            'data' => $request->data,
+            'order_status_id' => OrderStatus::query()->first()->id ?? 0,
+            'data' => $request->except('product_id'),
         ]);
 
-        return response()->json([
-            'message' => 'Fast order created successfully.',
-            'data' => $fastOrder,
-        ], 201);
+        return new ScmsResponse();
     }
 
-    public function form(Request $request)
+    public function form()
     {
         $fields = [];
         $formFields = setting('fastorder.fields', []);
